f687bfa8d2bbb82239fe5996e52c662acc29aaf2
Dan Kelosky <daniel.kelosky@ca.com>
2019-04-22 09:07:14 -0400

    mvp

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,22 @@
-import * as commander from "commander";
+#!/usr/bin/env node
+import { version, command, option, action, parse, help, Command } from "commander";
 
-// commander.
-console.log("@TEST");
+version(`0.0.1`);
+
+// command('rm <dir>')
+//   .option('-r, --recursive', 'Remove recursively')
+//   .action(function (dir: any, cmd: any) {
+//     console.log(`dir ${dir} cmd ${cmd}`)
+//   })
+
+command("init")
+  .description("initialize a project")
+  .action(() => {
+    console.log(`called init`)
+  });
+
+const cmd = parse(process.argv)
+
+if (cmd.args.length === 0) {
+  help();
+}
